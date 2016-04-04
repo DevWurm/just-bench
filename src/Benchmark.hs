74306e4d698bbc0bd1f1114e7b9ceaef21f51896
DevWurm <devwurm@gmx.net>
2016-04-04 23:17:02 +0200

    Adding the Benchmark module. The module contains functions, which are useful for all types of benchmarks.
    Adding the measuredAverage function to the Benchmark module. The function calculates a benchmark result, which is the average result for a series of benchmarks.

---
+++ src/Benchmark.hs
@@ -0,0 +1,29 @@
+module Benchmark
+    (
+    measuredAverage
+    ) where
+
+      import Criterion.Types
+      import Data.Int
+
+      {- Function for creating the average measurement results for one iteration
+       from an measurement m with n iterations -}
+      measuredAverage :: Int -> Measured -> Measured
+      measuredAverage n m = let
+                              fracN = realToFrac . fromIntegral $  n
+                            in Measured {
+                                measTime = measTime m / fracN,
+                                measCpuTime = measCpuTime m / fracN,
+                                measCycles = round $ int64ToFrac (measCycles m) / fracN,
+                                measIters = round $ int64ToFrac (measIters m) / fracN,
+                                measAllocated = round $ int64ToFrac (measAllocated m) / fracN,
+                                measNumGcs = round $ int64ToFrac (measNumGcs m) / fracN,
+                                measBytesCopied = round $ int64ToFrac (measBytesCopied m) / fracN,
+                                measMutatorWallSeconds = measMutatorWallSeconds m / fracN,
+                                measMutatorCpuSeconds = measMutatorCpuSeconds m / fracN,
+                                measGcWallSeconds = measGcWallSeconds m / fracN,
+                                measGcCpuSeconds = measGcCpuSeconds m / fracN
+                              }
+                      where
+                        int64ToFrac :: (Fractional a) => Int64 -> a
+                        int64ToFrac = realToFrac . fromIntegral
