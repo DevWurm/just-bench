198f4b758050c05d7a6631f65c56803b44aa8314
DevWurm <devwurm@gmx.net>
2016-04-11 20:07:23 +0200

    Moving measurement logic into seperate JustBench.Measure module.
    Generalize measurement by using typeclasses, so the same measurement logic can be used for different measurement processes.

--- src/JustBench/Measure.hs
+++ src/JustBench/Measure.hs
@@ -1,28 +1,55 @@
 module JustBench.Measure
     (
-    measuredAverage
+    BenchResult,
+    benchPure,
+    benchWeaklyPure
     ) where
 
-      import Criterion.Types
       import Data.Int
+      import qualified Criterion.Types as T
+      import Criterion.Main
+      import Criterion.Measurement
+      import Control.Monad.Par.Class
+
+      {- Class for types, which can contain elements of a measurement result and
+         which can be created from measurement results
+      -}
+      class BenchResult r where
+        fromMeasured :: T.Measured -> r
+
+      {- Create Benchmark of a pure function with normal form evaluation -}
+      benchPure :: (NFData b) => Int -> (a -> b) -> a -> IO T.Measured
+      benchPure it f arg = do
+                            let
+                              generalIt = fromIntegral it
+                            (result, _) <- nf f arg `measure` generalIt
+                            return $ measuredAverage it result
+
+      {- Create Benchmark of a pure function with weak head normal form evaluation -}
+      benchWeaklyPure :: Int -> (a -> b) -> a -> IO T.Measured
+      benchWeaklyPure it f arg = do
+                                 let
+                                    generalIt = fromIntegral it
+                                 (result, _) <- whnf f arg `measure` generalIt
+                                 return $ measuredAverage it result
 
       {- Function for creating the average measurement results for one iteration
        from an measurement m with n iterations -}
-      measuredAverage :: Int -> Measured -> Measured
+      measuredAverage :: Int -> T.Measured -> T.Measured
       measuredAverage n m = let
                               fracN = realToFrac . fromIntegral $  n
-                            in Measured {
-                                measTime = measTime m / fracN,
-                                measCpuTime = measCpuTime m / fracN,
-                                measCycles = round $ int64ToFrac (measCycles m) / fracN,
-                                measIters = round $ int64ToFrac (measIters m) / fracN,
-                                measAllocated = round $ int64ToFrac (measAllocated m) / fracN,
-                                measNumGcs = round $ int64ToFrac (measNumGcs m) / fracN,
-                                measBytesCopied = round $ int64ToFrac (measBytesCopied m) / fracN,
-                                measMutatorWallSeconds = measMutatorWallSeconds m / fracN,
-                                measMutatorCpuSeconds = measMutatorCpuSeconds m / fracN,
-                                measGcWallSeconds = measGcWallSeconds m / fracN,
-                                measGcCpuSeconds = measGcCpuSeconds m / fracN
+                            in T.Measured {
+                                T.measTime = T.measTime m / fracN,
+                                T.measCpuTime = T.measCpuTime m / fracN,
+                                T.measCycles = round $ int64ToFrac (T.measCycles m) / fracN,
+                                T.measIters = round $ int64ToFrac (T.measIters m) / fracN,
+                                T.measAllocated = round $ int64ToFrac (T.measAllocated m) / fracN,
+                                T.measNumGcs = round $ int64ToFrac (T.measNumGcs m) / fracN,
+                                T.measBytesCopied = round $ int64ToFrac (T.measBytesCopied m) / fracN,
+                                T.measMutatorWallSeconds = T.measMutatorWallSeconds m / fracN,
+                                T.measMutatorCpuSeconds = T.measMutatorCpuSeconds m / fracN,
+                                T.measGcWallSeconds = T.measGcWallSeconds m / fracN,
+                                T.measGcCpuSeconds = T.measGcCpuSeconds m / fracN
                               }
                       where
                         int64ToFrac :: (Fractional a) => Int64 -> a
