4a3be1cfa2f3cd1b63d9451c76ccda3094e73daf
DevWurm <devwurm@gmx.net>
2016-04-11 20:22:57 +0200

    Changing the JustBench.Measure.Time module.
    The module now contains only a data type for results of time measurements, which is in the BenchResult type class

--- src/JustBench/Measure/Time.hs
+++ src/JustBench/Measure/Time.hs
@@ -1,42 +1,23 @@
 module JustBench.Measure.Time
     (
-      benchPure,
-      benchWeaklyPure,
       TimeBenchResult (..)
     ) where
 
       import JustBench.Measure
-      import qualified Criterion.Types as T
-      import Criterion.Main
-      import Criterion.Measurement
-      import Control.Monad.Par.Class
+      import Criterion.Types
 
       data TimeBenchResult = TimeBenchResult {
                               time :: Double,
                               cpuTime :: Double,
                               cycles :: Integer,
-                              interations :: Integer
+                              iterations :: Integer
                              }
                              deriving (Show)
 
-      fromTimeMeasure :: T.Measured -> TimeBenchResult
-      fromTimeMeasure m = TimeBenchResult {
-                            time = T.measMutatorWallSeconds m,
-                            cpuTime = T.measMutatorCpuSeconds m,
-                            cycles = fromIntegral $ T.measCycles m,
-                            interations = fromIntegral $ T.measIters m
-                          }
-
-      benchPure :: (NFData b) => Int -> (a -> b) -> a -> IO TimeBenchResult
-      benchPure it f arg = do
-                            let
-                              generalIt = fromIntegral it
-                            (result, _) <- nf f arg `measure` generalIt
-                            return . fromTimeMeasure . measuredAverage it $ result
-
-      benchWeaklyPure :: Int -> (a -> b) -> a -> IO TimeBenchResult
-      benchWeaklyPure it f arg = do
-                                 let
-                                    generalIt = fromIntegral it
-                                 (result, _) <- whnf f arg `measure` generalIt
-                                 return . fromTimeMeasure . measuredAverage it $ result
+      instance BenchResult TimeBenchResult where
+        fromMeasured m = TimeBenchResult {
+                          time = measTime m,
+                          cpuTime = measCpuTime m,
+                          cycles = fromIntegral $ measCycles m,
+                          iterations = fromIntegral $ measIters m
+                         }
